74c9f511939214922dec05bf9b12ae1885ebbc56
rb <rkrb@tyfonmail.se>
2024-07-05 15:06:29 +0200

    6-8 done in part 2

--- wrong.js
+++ wrong.js
@@ -11,3 +11,10 @@ correctRecipeName.innerText = "Frozen Cheescake";
 
 const timeIcon = document.getElementsByClassName("time-container");
 timeIcon[0].firstElementChild.classList.add("material-icons");
+timeIcon[0].lastElementChild.innerText = "60+ min";
+
+const image = document.querySelector(".image-container img");
+image.src = "assets/frozen-cheesecake-slice.jpg";
+
+const ingredientContainer = document.querySelector(".how-to-do .ingredients-container");
+ingredientContainer.style.backgroundColor = "#f9f9f9";
