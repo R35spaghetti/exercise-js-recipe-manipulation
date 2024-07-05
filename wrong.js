b5dfe39b52d1d05938724c9785af58774d90bd19
rb <rkrb@tyfonmail.se>
2024-07-05 14:30:19 +0200

    1-2 done in part 2

---
+++ wrong.js
@@ -0,0 +1,7 @@
+const logoTextColour = document.getElementsByClassName("logo-text");
+logoTextColour[0].style.color = "#384241";
+
+const correctedHeader = document.getElementsByTagName("header")[0];
+correctedHeader.style.display = "flex";
+correctedHeader.style.justifyContent = "flex-start";
+correctedHeader.style.borderBottomColor = "lightgray";
