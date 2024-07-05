f76fb8bbe6371c00b9f4fe0229962c71e83fcac8
rb <rkrb@tyfonmail.se>
2024-07-05 14:48:28 +0200

    4-5 done in part 2 (3 already done last commit)

--- wrong.js
+++ wrong.js
@@ -5,3 +5,9 @@ const correctedHeader = document.getElementsByTagName("header")[0];
 correctedHeader.style.display = "flex";
 correctedHeader.style.justifyContent = "flex-start";
 correctedHeader.style.borderBottomColor = "lightgray";
+
+const correctRecipeName = document.getElementById("recipe-name");
+correctRecipeName.innerText = "Frozen Cheescake";
+
+const timeIcon = document.getElementsByClassName("time-container");
+timeIcon[0].firstElementChild.classList.add("material-icons");
