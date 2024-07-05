1ec984b74339dc570d8f44ca781ea6d43cfb3847
rb <rkrb@tyfonmail.se>
2024-07-05 15:41:44 +0200

    9-11 done in part 2

--- wrong.js
+++ wrong.js
@@ -11,10 +11,22 @@ correctRecipeName.innerText = "Frozen Cheescake";
 
 const timeIcon = document.getElementsByClassName("time-container");
 timeIcon[0].firstElementChild.classList.add("material-icons");
-timeIcon[0].lastElementChild.innerText = "60+ min";
+timeIcon[0].lastElementChild.textContent = "60+ min";
 
 const image = document.querySelector(".image-container img");
 image.src = "assets/frozen-cheesecake-slice.jpg";
 
 const ingredientContainer = document.querySelector(".how-to-do .ingredients-container");
 ingredientContainer.style.backgroundColor = "#f9f9f9";
+
+const ingredientsListBottom = document.querySelector(".ingredients-list-bottom");
+const correctContent = "<li>15st digistivetex</li><li>Lite smör</li>";
+ingredientsListBottom.innerHTML = correctContent;
+
+
+const ingredientsListPaste = document.querySelector(".ingredients-list-paste");
+const thirdIngredient = ingredientsListPaste.children[2];
+thirdIngredient.textContent = "3tsk vaniljsocker";
+const missingIngredientElement = document.createElement('li');
+missingIngredientElement.textContent = "400g naturell philadelphiaost";
+ingredientsListPaste.appendChild(missingIngredientElement);
